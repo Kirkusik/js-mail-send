ab30e53e125cb4e9fcf81b9f44e1678954101992
Roman Marchenko <kirkusik@gmail.com>
2020-01-23 14:23:07 +0200

    Update send-script.js

--- send-script.js
+++ send-script.js
@@ -1,25 +1,11 @@
-function submitHandler(e) {
-  e.preventDefault();
-
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() { 
-    console.log("readyState=", this.readyState, "statis=", this.status);
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // success, show this.responseText here
-        console.log("SUCCESS", this);
-    }
-  }
-  
-  request.open(this.method, this.action, true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  
-  var data = new FormData(this);
-  for (var key of data.keys())
-    console.log(key, data.get(key));
-    
-  request.send(data);
-}
-
-document.querySelectorAll("form").forEach(form =>
-  form.addEventListener("submit", submitHandler)
-);
+let form = document.querySelector('form')
+form.addEventListener('submit', submitHandler)
+function submitHandler(){
+  fetch("mail.php", {
+    method: "POST",
+    body: new FormData(form);
+  })
+  .then(response => response.json())
+  .then(function(json) { /* process your JSON further */ })
+  .catch(function(error) { console.log(error); });
+});
