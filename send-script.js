626373199ef96406d57fe2d0aa1ee49361046382
Roman Marchenko <kirkusik@gmail.com>
2020-01-30 11:18:14 +0200

    Update send-script.js

--- send-script.js
+++ send-script.js
@@ -9,3 +9,28 @@ function submitHandler(){
   .then(function(json) { /* process your JSON further */ })
   .catch(function(error) { console.log(error); });
 });
+
+//jquery
+formSubmit('#subscribesForm');
+	
+	function formSubmit(selector){
+		$(selector).on('submit', function(evt){
+			evt.preventDefault();
+			submitHandler(this.id);
+		});
+	};
+
+	function submitHandler(formId){
+		$.ajax({
+			url:"/send-mail.php", //url страницы (action_ajax_form.php)
+			type:"POST", //метод отправки
+			data: $('#'+formId).serialize(),  // Сеарилизуем объект
+			success: function(response) { //Данные отправлены успешно
+				document.getElementById(formId).reset();
+				console.log('response: '+response);
+			},
+			error: function(response) { // Данные не отправлены
+
+			}
+		});
+	};
