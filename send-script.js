b1826633c1ff4a616eec4cd9818e1153869cc276
Roman Marchenko <kirkusik@gmail.com>
2020-10-16 18:42:16 +0300

    Update send-script.js

--- send-script.js
+++ send-script.js
@@ -10,6 +10,21 @@ function submitHandler(){
   .catch(function(error) { console.log(error); });
 });
 
+
+var http = new XMLHttpRequest();
+var url = 'get_data.php';
+var params = 'orem=ipsum&name=binny';
+http.open('POST', url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
 //jquery
 formSubmit('#subscribesForm');
 	
